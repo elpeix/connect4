bca56bc48a39899252885d17a8ba232048003b6b
Francesc Requesens <francesc.requesens@gmail.com>
2023-02-03 19:06:56 +0100

    Simplified development with vite

---
+++ main.js
@@ -0,0 +1,4 @@
+import './src/connect4.css'
+import Connect4Game from './src/Connect4Game'
+
+Connect4Game(document.querySelector('#connect4'))
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import Connect4View from './Connect4View.js';
-import "./style.scss";
-
-const element = document.querySelector('#connect');
-Connect4View(element);
